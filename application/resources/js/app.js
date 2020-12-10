b717376f039c88a420f0fe7014da0fca14826a37
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-10 21:50:08 +0000

    pritter and enabling vuex

--- application/resources/js/app.js
+++ application/resources/js/app.js
@@ -1,26 +1,28 @@
-require('./bootstrap');
+require("./bootstrap");
 
-require('moment');
+require("moment");
 
-import Vue from 'vue';
+import Vue from "vue";
+import Vuex from "vuex";
 
-import { InertiaApp } from '@inertiajs/inertia-vue';
-import { InertiaForm } from 'laravel-jetstream';
-import PortalVue from 'portal-vue';
+import { InertiaApp } from "@inertiajs/inertia-vue";
+import { InertiaForm } from "laravel-jetstream";
+import PortalVue from "portal-vue";
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(Vuex);
 
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 
 new Vue({
-    render: (h) =>
+    render: h =>
         h(InertiaApp, {
             props: {
                 initialPage: JSON.parse(app.dataset.page),
-                resolveComponent: (name) => require(`./Pages/${name}`).default,
-            },
-        }),
+                resolveComponent: name => require(`./Pages/${name}`).default
+            }
+        })
 }).$mount(app);
