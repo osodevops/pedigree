58fcfed553d9dc574ccba6c5dc26a161d4a7c1b6
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-10 21:50:43 +0000

    base panel

---
+++ application/resources/js/Components/Base/Panel.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
