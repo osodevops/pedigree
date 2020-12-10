4382f739dbe295aa9e8616699765dbb7d9626675
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-10 21:50:53 +0000

    Search bar component

---
+++ application/resources/js/Components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="w-full p-4">
+        <jet-input
+            class="w-full"
+            v-model="githubUrl"
+            placeholder="github repo url"
+        ></jet-input>
+    </div>
+</template>
+
+<script>
+import JetInput from "@/Jetstream/Input";
+
+export default {
+    components: {
+        JetInput
+    },
+    data() {
+        return {
+            githubUrl: ""
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
