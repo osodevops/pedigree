879da8cd305c8972a34e695609e58fb6d99094e4
Richard Toms <richard@toms.dev>
2020-12-12 21:15:58 +0000

    Update SocialiteController.php

--- application/app/Http/Controllers/Auth/SocialiteController.php
+++ application/app/Http/Controllers/Auth/SocialiteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController
@@ -24,6 +25,7 @@ class SocialiteController
             'profile_photo_path' => $authenticated->getAvatar()
         ]);
 
+        Cache::put(get_class($user) . ":{$user->id}.github.token", $authenticated->token);
         Auth::login($user);
         return redirect()->to('dashboard');
     }
