b4bf51fbb842c5b824ab03bdd4018874bad94137
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-12 16:09:38 +0000

    added button to search bar

--- application/resources/js/Components/SearchBar.vue
+++ application/resources/js/Components/SearchBar.vue
@@ -1,24 +1,47 @@
 <template>
     <div class="w-full p-4">
-        <jet-input
-            class="w-full"
-            v-model="githubUrl"
-            placeholder="github repo url"
-        ></jet-input>
+        <div class="flex flex-wrap">
+            <jet-input
+                class="w-8/12 mr-2"
+                v-model="githubUrl"
+                placeholder="github repo url"
+            ></jet-input>
+            <jet-button
+                type="button"
+                class="w-2/12 text-center"
+                @click="searchRepos()"
+                >Search</jet-button
+            >
+        </div>
+        <jet-input-error></jet-input-error>
     </div>
 </template>
 
 <script>
 import JetInput from "@/Jetstream/Input";
+import JetButton from "@/Jetstream/Button";
+import JetInputError from "@/Jetstream/InputError";
 
 export default {
     components: {
-        JetInput
+        JetInput,
+        JetButton,
+        JetInputError
     },
     data() {
         return {
             githubUrl: ""
         };
+    },
+    computed: {
+        errorMessage() {
+            //
+        }
+    },
+    methods: {
+        searchRepos() {
+            //
+        }
     }
 };
 </script>
