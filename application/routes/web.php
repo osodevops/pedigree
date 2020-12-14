6952d8ea36af476ddfae64ee7ff24395beb530c3
Richard Toms <richard@toms.dev>
2020-12-14 21:04:34 +0000

    Redirecting register / login to the github registration

--- application/routes/web.php
+++ application/routes/web.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\ForkController;
 use App\Http\Controllers\RepositoryController;
 use App\Http\Controllers\Auth\SocialiteController;
 
-Route::view('/', 'welcome');
-$router->get('auth/{provider}/redirect', [SocialiteController::class, 'index']);
-$router->get('auth/{provider}/callback', [SocialiteController::class, 'show']);
+$router->view('/', 'welcome');
+
+$router->get('auth/{provider}/redirect', [SocialiteController::class, 'index'])->name('auth.socialite.register');
+$router->get('auth/{provider}/callback', [SocialiteController::class, 'show'])->name('auth.socialite.login');
 
 Route::group(['middleware' => ['auth:sanctum']], function ($router) {
     $router->get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
     $router->get('repos/{user}/{repository}', [RepositoryController::class, 'show']);
     $router->get('repos/{user}/{repository}/forks', [ForkController::class, 'index']);
 });
+
+$router->redirect('register', '/auth/github/redirect');
+$router->redirect('login', '/auth/github/redirect');
