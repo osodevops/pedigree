84f767c4fea1d481b177c34109315fdb7ba27c26
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-16 21:32:41 +0000

    base store and types file

--- application/resources/js/Store/store.js
+++ application/resources/js/Store/store.js
@@ -1,3 +1,15 @@
+import Vue from "vue";
 import Vuex from "vuex";
+import * as Types from "./types";
+Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+    state: {
+        user: {}
+    },
+    mutations: {
+        [Types.STORE_UPDATE_USER](state, user) {
+            state.user = user;
+        }
+    }
+});
