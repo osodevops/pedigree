4b39f6b3de38994ec8282f373218b33bbdd72661
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-17 22:10:59 +0000

    code clean up

--- application/resources/js/Components/RepoDetailView.vue
+++ application/resources/js/Components/RepoDetailView.vue
@@ -2,11 +2,20 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
             <h1>RepoDetailView component</h1>
-            <slot> </slot>
+            <base-container>
+                <!-- content -->
+            </base-container>
         </div>
     </div>
 </template>
 <script>
+import BaseContainer from "@/Components/Base/Container";
+
 export default {
-}
+    components: {
+        BaseContainer
+    }
+};
 </script>
+
+<style lang="scss" scoped></style>
--- application/resources/js/Pages/Dashboard.vue
+++ application/resources/js/Pages/Dashboard.vue
@@ -5,69 +5,35 @@
                 Dashboard
             </h2>
         </template>
-        <base-panel>
-            <search-bar></search-bar>
-
-            <base-card @click.native="activateCanSee()">
-                <jet-button>see full section</jet-button>
-            </base-card>
-            <base-card>
-                <h1>rendering card</h1>
-            </base-card>
-            <base-card>
-                <h1>rendering card</h1>
-            </base-card>
-            <base-card>
-                <h1>rendering card</h1>
-            </base-card>
-        </base-panel>
-        <repo-detail-view v-show="canSee">
-              <repo-detail-layout> </repo-detail-layout>
-        </repo-detail-view>
         <component :is="activeComponent"></component>
-
     </app-layout>
 </template>
 
 <script>
 import { mapGetters, mapMutations, mapState } from "vuex";
-import AppLayout from "@/Layouts/AppLayout";
-import SearchBar from "@/Components/SearchBar.vue";
-import BasePanel from "@/Components/Base/Panel";
-import BaseCard from "@/Components/Base/Card";
-import JetButton from "@/Jetstream/Button";
 import RepoDetailView from "@/Components/RepoDetailView.vue";
-import RepoDetailLayout from "@/Components/Base/Container.vue";
 import dashboardModule from "@/Store/Modules/Dashboard/index";
 import searchRepos from "@/components/SearchRepos";
 
 export default {
     components: {
-        AppLayout,
-        SearchBar,
-        BasePanel,
-        BaseCard,
-        JetButton,
         RepoDetailView,
-        RepoDetailLayout
         searchRepos
     },
     props: {
-        user: Object,
+        user: Object
     },
     computed: {
-    },
-    methods: {
-        activateCanSee() {
-            this.canSee = !this.canSee
         storeDashboard() {
             return this.$store.state.dashboard;
         },
         activeComponent() {
             if (this.storeDashboard) {
                 switch (this.storeDashboard.stage) {
-                    case "dashboard-search-component":
+                    case "dashboard-search":
                         return searchRepos;
+                    case "dashboard-repo-detail-view":
+                        return RepoDetailView;
                 }
             }
         }
