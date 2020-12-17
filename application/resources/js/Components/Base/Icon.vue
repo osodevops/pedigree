c7a080a984af8842e276d45cc95b3ad1770447ed
Richard Toms <richard@toms.dev>
2020-12-17 14:05:51 +0000

    Created icon component & imported FontAwesome

---
+++ application/resources/js/Components/Base/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <span class="fas" :class="classes"></span>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            default: ""
+        },
+        size: {
+            type: String,
+            default: "sm"
+            validator: function (value) {
+                return ['xs', 'sm', 'lg', '2x', '3x', '5x', '7x', '10x'].indexOf(value) !== -1
+            }
+        },
+    },
+    computed: {
+        classes() {
+            return [this.prefix(this.icon), this.prefix(this.size)].join(' ');
+        }
+    },
+    methods: {
+        prefix(value) {
+            return `fa-${value}`;
+        }
+    }
+};
+</script>
