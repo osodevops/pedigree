5d697fb9a4d8dc9126c5fb1e99bf17974688cb57
Richard Toms <richard@toms.dev>
2020-12-18 22:24:44 +0000

    Update RepoDetailView.vue

--- application/resources/js/Components/RepoDetailView.vue
+++ application/resources/js/Components/RepoDetailView.vue
@@ -1,24 +1,31 @@
 <template>
     <div>
-        <div class="flex">
+        <div class="flex mb-4">
             <div style="width: 50px; height:50px" class="mr-4">
                 <img :src="repository.owner.avatar_url">
             </div>
             <div class="flex flex-col">
                 <span class="text-lg font-semibold">{{repository.name}}</span>
-                <span>{{repository.owner.name}}</span>
+                <div class="flex items-center">
+                    <span>{{repository.owner.name}}</span>
+                    <div class="flex items-center ml-4">
+                        <Icon icon="code-branch"/>
+                        <span class="ml-2">{{repository.default_branch}}</span>
+                    </div>
+                </div>
                 <p class="italic">{{repository.description}}</p>
             </div>
         </div>
-
     </div>
 </template>
 <script>
 import BaseCard from "@/Components/Base/Card";
+import Icon from "@/Components/Base/Icon";
 
 export default {
     components: {
-        BaseCard
+        BaseCard,
+        Icon
     },
     props: {
         repository: Object
