f5ed26845c038a2dd5c76f3031d75cabf335f0ae
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-19 17:22:08 +0000

    adjusted shadow on panel

--- application/resources/js/Components/Base/Panel.vue
+++ application/resources/js/Components/Base/Panel.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
                 <slot></slot>
             </div>
         </div>
