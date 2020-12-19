ed918677ceff78efdd0069060b10e42046a41c1f
Jamie Callis <jamiecallis2014@gmail.com>
2020-12-19 19:24:50 +0000

    styling and extension

--- application/resources/js/Components/RepoDetailView.vue
+++ application/resources/js/Components/RepoDetailView.vue
@@ -1,21 +1,48 @@
 <template>
     <base-container>
         <div class="flex mb-4">
-            <div style="width: 50px; height:50px" class="mr-4">
+            <div style="width: 75px; height:75px" class="mr-4">
                 <img :src="repository.owner.avatar_url" />
             </div>
             <div class="flex flex-col">
-                <span class="text-lg font-semibold">{{ repository.name }}</span>
+                <span class="text-2xl font-semibold">{{
+                    repository.name
+                }}</span>
                 <div class="flex items-center">
-                    <span>{{ repository.owner.name }}</span>
+                    <span class="font-bold">{{ repository.owner.name }}</span>
                     <div class="flex items-center ml-4">
-                        <Icon icon="code-branch" />
-                        <span class="ml-2">{{
+                        <Icon icon="code-branch" class="text-gray-400" />
+                        <span class="ml-2 font-bold">{{
                             repository.default_branch
                         }}</span>
                     </div>
+                    <div class="flex items-center ml-4">
+                        <Icon icon="code-branch" class="text-gray-400" />
+                        <span class="ml-2"
+                            ><span class="font-bold">{{
+                                repository.forks.total
+                            }}</span>
+                            forks</span
+                        >
+                    </div>
+                    <div class="flex items-center ml-4">
+                        <Icon icon="star" class="text-gray-400" />
+                        <span class="ml-2"
+                            ><span class="font-bold">{{
+                                repository.activity.watchers_count
+                            }}</span>
+                            stars</span
+                        >
+                    </div>
                 </div>
-                <p class="italic">{{ repository.description }}</p>
+                <div class="mt-2">
+                    <p class="italic">{{ repository.description }}</p>
+                    <a
+                        class="italic text-blue-500 hover:text-blue-400"
+                        :href="repository.url"
+                        >{{ repository.url }}</a
+                    >
+                </div>
             </div>
         </div>
     </base-container>
