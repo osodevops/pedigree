9c7af93bd77b893852ca74a32e1b905b5c8ac983
Jamie Callis <jamiecallis2014@gmail.com>
2021-01-07 19:33:13 +0000

    simplify vuex

--- application/resources/js/Store/Modules/SearchRepos/index.js
+++ application/resources/js/Store/Modules/SearchRepos/index.js
@@ -1,6 +1,4 @@
 import * as Types from "./types";
-import mutations from "./mutations";
-import * as actions from "./actions";
 
 export default {
     namespaced: true,
@@ -12,6 +10,80 @@ export default {
         forkData: [],
         loadingGithubInfo: false
     },
-    mutations,
-    actions
+    mutations: {
+        [Types.SEARCH_REPOS_UPDATE_REPOSITORY_BREAKDOWN](
+            state,
+            repositoryBreakdown
+        ) {
+            state.repositoryBreakdown = repositoryBreakdown;
+        },
+        [Types.SEARCH_REPOS_UPDATE_ERROR_MESSAGE](state, message) {
+            state.errorMessage = message;
+        },
+        [Types.SEARCH_REPOS_UPDATE_GITHUB_USERNAME](state, username) {
+            state.githubUsername = username;
+        },
+        [Types.SEARCH_REPOS_UPDATE_GITHUB_REPOSITORY_NAME](state, repoName) {
+            state.githubRepositoryName = repoName;
+        },
+        [Types.SEARCH_REPOS_UPDATE_FORKS_INFORMATION](state, forkData) {
+            state.forkData = forkData;
+        },
+        [Types.SEARCH_REPO_LOADING_STATE](state, isLoading) {
+            state.loadingGithubInfo = isLoading;
+        }
+    },
+    actions: {
+        getRepositoryInformation({ commit, state, dispatch }, url) {
+            let matches = [
+                ...url.matchAll(
+                    /(https:\/\/github\.com\/)?([\w\d-]*)\/([\w\d-]*)(.*)?/g
+                )
+            ];
+            const [username, repository] = [matches[0][2], matches[0][3]];
+
+            commit(Types.SEARCH_REPOS_UPDATE_GITHUB_USERNAME, username);
+            commit(
+                Types.SEARCH_REPOS_UPDATE_GITHUB_REPOSITORY_NAME,
+                repository
+            );
+
+            commit(Types.SEARCH_REPO_LOADING_STATE, true);
+
+            axios
+                .get(
+                    `/repos/${state.githubRepositoryName}/${state.githubRepositoryName}`
+                )
+                .then(
+                    ({ data }) => {
+                        commit(
+                            Types.SEARCH_REPOS_UPDATE_REPOSITORY_BREAKDOWN,
+                            data
+                        );
+                        commit(Types.SEARCH_REPO_LOADING_STATE, false);
+                        dispatch("getRepositoryForkInformation");
+                    },
+                    error => {
+                        commit(Types.SEARCH_REPOS_UPDATE_ERROR_MESSAGE, error);
+                    }
+                );
+        },
+        getRepositoryForkInformation({ commit, state }) {
+            axios
+                .get(
+                    `/repos/${state.githubUsername}/${state.githubRepositoryName}/forks`
+                )
+                .then(
+                    ({ data }) => {
+                        commit(
+                            Types.SEARCH_REPOS_UPDATE_FORKS_INFORMATION,
+                            data
+                        );
+                    },
+                    error => {
+                        commit(Types.SEARCH_REPOS_UPDATE_ERROR_MESSAGE, error);
+                    }
+                );
+        }
+    }
 };
