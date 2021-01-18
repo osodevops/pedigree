76d8e4c469fd5becb684880b76d056d7d5e367b0
Jamie Callis <jamiecallis2014@gmail.com>
2021-01-18 23:42:30 +0000

    histogram chart, and layout adjustments

--- application/resources/js/Components/RepoDetailView.vue
+++ application/resources/js/Components/RepoDetailView.vue
@@ -4,68 +4,74 @@
             Repository Overview
         </h1>
 
-        <base-container>
-            <div class="flex mb-4">
-                <!-- Repository Information -->
-                <div style="width: 75px; height:75px" class="mr-4">
-                    <img :src="repository.owner.avatar_url" />
-                </div>
-                <div class="flex flex-col">
-                    <span class="text-2xl font-semibold">{{
-                        repository.name
-                    }}</span>
-                    <div class="flex items-center">
-                        <span class="font-bold">{{
-                            repository.owner.name
+        <div class="mx-auto flex flex-wrap w-3/4">
+            <base-container>
+                <div class="flex mb-4">
+                    <!-- Repository Information -->
+                    <div style="width: 75px; height:75px" class="mr-4">
+                        <img :src="repository.owner.avatar_url" />
+                    </div>
+                    <div class="flex flex-col">
+                        <span class="text-2xl font-semibold">{{
+                            repository.name
                         }}</span>
-                        <div class="flex items-center ml-4">
-                            <Icon icon="code-branch" class="text-gray-400" />
-                            <span class="ml-2 font-bold">{{
-                                repository.default_branch
+                        <div class="flex items-center">
+                            <span class="font-bold">{{
+                                repository.owner.name
                             }}</span>
                         </div>
-                        <div class="flex items-center ml-4">
-                            <Icon icon="code-branch" class="text-gray-400" />
-                            <span class="ml-2"
-                                ><span class="font-bold">{{
-                                    repository.forks.total
-                                }}</span>
-                                forks</span
-                            >
+                        <div class="mt-2">
+                            <p class="italic">{{ repository.description }}</p>
                         </div>
-                        <div class="flex items-center ml-4">
-                            <Icon icon="star" class="text-gray-400" />
-                            <span class="ml-2"
-                                ><span class="font-bold">{{
-                                    repository.activity.watchers_count
-                                }}</span>
-                                stars</span
-                            >
-                        </div>
-                    </div>
-                    <div class="mt-2">
-                        <p class="italic">{{ repository.description }}</p>
-                        <a
-                            class="italic text-blue-500 hover:text-blue-400"
-                            :href="repository.url"
-                            >{{ repository.url }}</a
-                        >
+
+                        <HistorgramGraph />
                     </div>
                 </div>
+            </base-container>
+            <div class="w-1/4 p-4">
+                <a
+                    class="italic text-blue-500 hover:text-blue-400"
+                    :href="repository.url"
+                    >{{ repository.url }}</a
+                >
+                <div class="flex items-center">
+                    <Icon icon="code-branch" class="text-gray-400" />
+                    <span class="font-bold ml-2">{{
+                        repository.default_branch
+                    }}</span>
+                </div>
+                <div class="flex items-center">
+                    <Icon icon="code-branch" class="text-gray-400" />
+                    <span class="ml-2">
+                        <span class="font-bold">{{
+                            repository.forks.total
+                        }}</span>
+                        forks
+                    </span>
+                </div>
+                <div class="flex items-center">
+                    <Icon icon="star" class="text-gray-400" />
+                    <span class="ml-2"
+                        ><span class="font-bold">{{
+                            repository.activity.watchers_count
+                        }}</span>
+                        stars</span
+                    >
+                </div>
             </div>
-        </base-container>
+        </div>
 
         <h1
             v-show="forkData.length > 0"
-            class="w-3/4 mx-auto text-2xl mt-2 mb-2 text-gray-700"
+            class="w-3/4 mx-auto text-2xl mt-3 mb-2 text-gray-700"
         >
             Fork Information
         </h1>
 
         <!-- fork data section -->
-        <div class="mt-6 w-3/4 mx-auto flex flex-wrap">
-            <div v-for="fork in forkData" :key="fork.id" class="w-1/2 p-2">
-                <div class="flex p-2 bg-white rounded shadow-md">
+        <div class="mt-3 w-3/4 mx-auto flex flex-wrap">
+            <div v-for="fork in forkData" :key="fork.id" class="w-full">
+                <div class="flex p-4 bg-white rounded shadow-md">
                     <div style="width: 75px; height:75px" class="mr-4">
                         <img :src="fork.owner.avatar_url" />
                     </div>
@@ -192,6 +198,8 @@
                             </table>
                         </div>
                     </div>
+
+                    <HistorgramGraph />
                 </div>
             </div>
         </div>
@@ -201,12 +209,14 @@
 import BaseContainer from "@/Components/Base/Container";
 import BaseCard from "@/Components/Base/Card";
 import Icon from "@/Components/Base/Icon";
+import HistorgramGraph from "@/Components/HistorgramGraph";
 
 export default {
     components: {
         BaseContainer,
         BaseCard,
-        Icon
+        Icon,
+        HistorgramGraph
     },
     props: {
         repository: Object,
