f36db180e7fffc5b71f6ff4057787d54b314292e
Richard Toms <richard@toms.dev>
2021-01-22 21:31:55 +0000

    Removed Socialite & clean up

--- application/app/Providers/AppServiceProvider.php
+++ application/app/Providers/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->singleton(GitHub::class, function () {
-            return new GitHub(config('services.github.key'));
-        });
+        //
     }
 }
