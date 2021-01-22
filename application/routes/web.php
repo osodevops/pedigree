41c039d3ab2cb6abf8cd496d0812c7e97b432a7e
Richard Toms <richard@toms.dev>
2021-01-22 21:24:57 +0000

    Removal of auth middleware & requirement

--- application/routes/web.php
+++ application/routes/web.php
@@ -5,23 +5,16 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ForkController;
 use App\Http\Controllers\RepositoryController;
 use App\Http\Controllers\Statistics;
-use App\Http\Controllers\Auth\SocialiteController;
 
-$router->view('/', 'welcome');
+$router->get('/', [DashboardController::class, 'show'])->name('dashboard');
 
-$router->get('auth/{provider}/redirect', [SocialiteController::class, 'index'])->name('auth.socialite.register');
-$router->get('auth/{provider}/callback', [SocialiteController::class, 'show'])->name('auth.socialite.login');
+$router->group(['prefix' => 'repos/{user}/{repository}'], function ($router) {
+    $router->get('/', [RepositoryController::class, 'show']);
+    $router->get('/forks', [ForkController::class, 'index']);
+    $router->get('/differences', [DifferenceController::class, 'index']);
 
-Route::group(['middleware' => ['auth:sanctum']], function ($router) {
-    $router->get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
-    $router->get('repos/{user}/{repository}', [RepositoryController::class, 'show']);
-    $router->get('repos/{user}/{repository}/forks', [ForkController::class, 'index']);
-
-    $router->get('repos/{user}/{repository}/stats/commits', [Statistics\CommitController::class, 'show'])
+    $router->get('/stats/commits', [Statistics\CommitController::class, 'show'])
         ->middleware(['cache.response']);
-    $router->get('repos/{user}/{repository}/stats/languages', [Statistics\LanguageController::class, 'show'])
+    $router->get('/stats/languages', [Statistics\LanguageController::class, 'show'])
         ->middleware(['cache.response']);
 });
-
-$router->redirect('register', '/auth/github/redirect');
-$router->redirect('login', '/auth/github/redirect');
