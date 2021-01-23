70b78b17618d0e95969a51cefe44a7cf491c9c02
Richard Toms <richard@toms.dev>
2021-01-23 00:38:02 +0000

    Created a smart container to scroll off-screen on smaller devices

---
+++ application/resources/js/Components/HeatmapContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="md:mx-2 mx-3 flex flex-col items-end xl:items-center overflow-hidden pt-1 h-full text-center">
+        <commits-heatmap :data="data" />
+    </div>
+</template>
+
+<script>
+import CommitsHeatmap from "@/Components/CommitsHeatmap";
+
+export default {
+    components: {
+        CommitsHeatmap
+    },
+    props: {
+        data: Array
+    }
+}
+</script>
