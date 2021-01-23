0cdb603e38ecfdf92fe8e8ba50b9fb47db066e58
Richard Toms <richard@toms.dev>
2021-01-23 16:57:03 +0000

    Styled closer to GitHub's implementation

--- application/resources/js/Components/Diff.vue
+++ application/resources/js/Components/Diff.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="flex justify-center items-center">
+    <div class="flex justify-center items-center w-44">
         <div class="flex items-end flex-1 flex-col">
-            <span class="mr-2 text-gray-800 text-xs mb-1">{{ behindBy }}</span>
+            <span class="mr-2 text-gray-800" style="font-size: 10px">{{ behindBy }}</span>
             <div class="w-full flex justify-end">
-                <div style="height: 5px;" :style="{width: behindPercentage}" class="bg-gray-300 rounded-tl-full rounded-bl-full"></div>
+                <div style="height: 4px;" :style="{width: behindPercentage}" class="bg-gray-300 rounded-tl-full rounded-bl-full"></div>
             </div>
         </div>
-        <div class="h-10 bg-gray-500" style="width:1px"></div>
+        <div class="h-5 bg-gray-500" style="width:1px"></div>
         <div class="flex flex-1 flex-col">
-            <span class="ml-2 text-gray-800 text-xs mb-1">{{ aheadBy }}</span>
+            <span class="ml-2 text-gray-800" style="font-size: 10px">{{ aheadBy }}</span>
             <div class="w-full">
-                <div style="height: 5px" :style="{width: aheadPercentage}" class="bg-gray-300 rounded-tr-full rounded-br-full"></div>
+                <div style="height: 4px" :style="{width: aheadPercentage}" class="bg-gray-300 rounded-tr-full rounded-br-full"></div>
             </div>
         </div>
     </div>
@@ -37,11 +37,15 @@ export default {
             return Math.max(this.behindBy, this.aheadBy, +this.max, 1);
         },
         aheadPercentage() {
-            const perc = Math.floor((this.aheadBy / this.maxScale) * 100);
+            const perc = Math.min(
+                100, Math.floor((this.aheadBy / this.maxScale) * 100) + 2
+            );
             return `${perc}%`;
         },
         behindPercentage() {
-            const perc = Math.floor((this.behindBy / this.maxScale) * 100);
+            const perc = Math.min(
+                100, Math.floor((this.behindBy / this.maxScale) * 100) + 2
+            );
             return `${perc}%`;
         }
     }
