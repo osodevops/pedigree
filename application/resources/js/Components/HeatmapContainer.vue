fe38abd215b14ab7f12e14b96a8f609ab3894fc7
Richard Toms <richard@toms.dev>
2021-01-24 17:51:47 +0000

    Retry mechanic for commits heatmap

--- application/resources/js/Components/HeatmapContainer.vue
+++ application/resources/js/Components/HeatmapContainer.vue
@@ -2,8 +2,12 @@
     <div class="max-w-full w-full">
         <h2 class="text-black text-lg mb-2">{{ numberFormat(total) }} contributions in the last year</h2>
         <base-card>
-            <div class="flex flex-col items-end xl:items-center overflow-hidden h-full text-center">
+            <BaseLoadingSpinner v-show="loading" />
+            <div class="flex flex-col items-end xl:items-center overflow-hidden h-full text-center" v-if="!loading && success">
                 <commits-heatmap :data="commits" />
+            </div>
+            <div v-else class="flex justify-center items-center p-4 text-sm text-gray-700">
+                <div v-if="!loading">Commit history could not be loaded. <a class="text-blue-500 cursor-pointer hover:underline" @click="load()">Retry</a></div>
             </div>
         </base-card>
     </div>
@@ -12,11 +16,13 @@
 <script>
 import CommitsHeatmap from "@/Components/CommitsHeatmap";
 import BaseCard from "@/Components/Base/Card";
+import BaseLoadingSpinner from "@/Components/Base/LoadingSpinner";
 
 export default {
     components: {
         CommitsHeatmap,
         BaseCard,
+        BaseLoadingSpinner,
     },
     props: {
         username: {
@@ -32,20 +38,31 @@ export default {
         return {
             commits: [],
             total: 0,
+            loading: false,
+            success: false,
         };
     },
     methods: {
         numberFormat(number) {
             return window.numberFormat(number);
+        },
+        load() {
+            this.loading = true;
+            axios
+                .get(`/repos/${this.username}/${this.repository}/stats/commits`)
+                .then(({ data }) => {
+                    this.total = data.data.total;
+                    this.commits = data.data.commits;
+                    this.success = true;
+                    this.loading = false;
+                }, () => {
+                    this.success = false;
+                    this.loading = false;
+                });
         }
     },
     mounted() {
-        axios
-            .get(`/repos/${this.username}/${this.repository}/stats/commits`)
-            .then(({ data }) => {
-                this.total = data.data.total;
-                this.commits = data.data.commits;
-            });
+        this.load();
     },
 };
 </script>
