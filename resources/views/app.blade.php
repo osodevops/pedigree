4e55a8a7e92c80ddddd9340a54cb55840b7c52ad
jjcallis <joshuajordancallis@gmail.com>
2021-03-07 21:31:03 +0000

    enable gtag on production only - don't merge until GTAG with correct value is added to the param store and add to the app manifest

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,3 +22,17 @@
         @inertia
     </body>
 </html>
+
+@if (app()->environment('production')))
+    <script> let GTAG = '{{ env('GTAG') }}'; </script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-`${GTAG}`"></script>
+    <script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', "G-`${GTAG}`");
+    </script>
+@endif
