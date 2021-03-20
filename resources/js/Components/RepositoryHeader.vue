f8e84430c77a987884aa05cf415a7bf15ce69f69
Richard Toms <richard@toms.dev>
2021-03-20 16:32:23 +0000

    Added the ability to view the repository in a new tab by clicking name/image

--- resources/js/Components/RepositoryHeader.vue
+++ resources/js/Components/RepositoryHeader.vue
@@ -2,13 +2,17 @@
     <div>
         <div class="flex flex-col sm:flex-row mb-4">
             <div style="width: 75px; height:75px" class="mr-4">
-                <img :src="repository.owner.avatar_url" />
+                <a class="block w-full" :href="`https://gihub.com/${repository.owner.id}/${repository.name}`" target="_blank">
+                    <img :src="repository.owner.avatar_url" />
+                </a>
             </div>
             <div class="flex flex-col">
-                <h1 class="flex flex-col sm:flex-row mb-2">
-                    <span class="text-md sm:text-xl">{{ repository.owner.id }}</span>
-                    <span class="text-xl mx-2 hidden sm:block">/</span>
-                    <span class="text-xl font-semibold">{{ repository.name }}</span>
+                <h1 class="mb-2">
+                    <a class="flex flex-col sm:flex-row" :href="`https://gihub.com/${repository.owner.id}/${repository.name}`" target="_blank">
+                        <span class="text-md sm:text-xl">{{ repository.owner.id }}</span>
+                        <span class="text-xl px-2 hidden sm:block">/</span>
+                        <span class="text-xl font-semibold">{{ repository.name }}</span>
+                    </a>
                 </h1>
                 <div class="mb-2">
                     <p class="text-sm text-gray-700">{{ repository.description }}</p>
